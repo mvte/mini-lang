759620c8969e659488585bc6ab24eeba9ad51adf
jan <57322982+mvte@users.noreply.github.com>
2025-04-06 23:13:03 -0400

    parser now returns a list of statements

--- include/parser.hpp
+++ include/parser.hpp
@@ -3,4 +3,4 @@
 #include <vector>
 #include <memory>
 
-std::shared_ptr<ASTNode> parse(const std::vector<Token>& tokens);
+std::vector<std::shared_ptr<ASTNode>> parse(const std::vector<Token>& tokens);
